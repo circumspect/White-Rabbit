dd6722e80d7d1bed27e1e996a30b758f3e464cc2
Creonalia <hyper.pyromaniac@simplelogin.co>
2020-09-18 17:14:25 -0700

    added extensions

--- __main__.py
+++ __main__.py
@@ -1,8 +1,9 @@
-import AiMDB
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix="!")
-bot.add_cog(AiMDB.AliceIsMissing(bot))
+
+for extension in ["AiMDB", "dev"]:
+    bot.load_extension(extension)
 
 with open("token.txt") as token_file:
     token = token_file.read()
