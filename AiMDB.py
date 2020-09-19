7c295e10953624cfcdaf544ebbdf33b86b449afd
Creonalia <hyper.pyromaniac@simplelogin.co>
2020-09-18 19:13:55 -0700

    timer and quit command

--- AiMDB.py
+++ AiMDB.py
@@ -2,15 +2,19 @@ import discord
 import time
 import random
 import asyncio
-from discord.ext import commands
+from discord.ext import commands, tasks
 
 
 class AliceIsMissing(commands.Cog):
-    characters = ("charlie barnes", "dakota travis",
-                  "evan holwell", "jack briarwood", "julia north")
+    characters = (
+        "charlie barnes", "dakota travis",
+        "evan holwell", "jack briarwood", "julia north"
+    )
+    game_length = 90 * 60
 
     def __init__(self, bot):
         self.bot = bot
+        self.setup = False
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -18,18 +22,33 @@ class AliceIsMissing(commands.Cog):
 
     @commands.Cog.listener()
     async def on_ready(self):
+        self.text_channels = {
+            channel.name: channel
+            for channel in self.bot.guilds[0].text_channels
+        }
         print(f"Bot has logged in as {self.bot}")
 
     @commands.command()
     async def start(self, ctx):
-        start_time = time.time()
+        if not self.setup:
+            await ctx.send("You have not setup")
+            return
+        self.start_time = time.time()
+        self.timer.start()
         await ctx.send('Starting the game!')
+
+    @tasks.loop(seconds=10)
+    async def timer(self):
+        if self.start_time + self.game_length < time.time():
+            self.timer.cancel()
+
+        remaining_time = self.start_time + self.game_length - time.time()
+        await self.text_channels["group-chat"].send((
+            f"{str(int(remaining_time // 60)).zfill(2)}:{str(int(remaining_time % 60)).zfill(2)}"
+        ))
 
     @commands.command()
     async def setup(self, ctx):
-        self.text_channels = {
-            channel.name: channel for channel in ctx.guild.text_channels}
-
         # send character cards
         motives = list(range(1, 6))
         random.shuffle(motives)
@@ -41,6 +60,8 @@ class AliceIsMissing(commands.Cog):
             asyncio.ensure_future(channel.send(file=discord.File(
                 f"Images/Cards/Motives/Motive {motives[i]}.png"
             )))
+        self.setup = True
+        await ctx.send("Done setting up")
 
 
 def setup(bot):
