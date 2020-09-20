0829f0eec76b2c2f179bc5ee70429c7201563b95
Creonalia <hyper.pyromaniac@simplelogin.co>
2020-09-20 13:24:36 -0700

    move games into bot

--- __main__.py
+++ __main__.py
@@ -1,12 +1,23 @@
 import discord
+import gamedata
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix="!")
+bot.games = {}
+
 
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Game("Alice is Missing"))
 
+
+@bot.before_invoke
+async def before_invoke(ctx):
+    ctx.game = bot.games.setdefault(ctx.guild.id, gamedata.Data(ctx.guild))
+    ctx.text_channels = {
+        channel.name: channel
+        for channel in ctx.guild.text_channels
+    }
 
 for extension in ["admin", "game", "players"]:
     bot.load_extension(extension)
