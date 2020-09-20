e86d255836e17d404b60f5daf842767d6da25211
Creonalia <hyper.pyromaniac@simplelogin.co>
2020-09-19 20:00:27 -0700

    check for players with roles on init

--- gamedata.py
+++ gamedata.py
@@ -15,10 +15,15 @@ GAME_LENGTH = 90 * 60
 # How often the bot should check the timer
 TIMER_GAP = 10
 
+
 class Data:
     def __init__(self, guild):
         self.guild = guild
         self.setup = False
         self.started = False
         self.show_timer = False
-        self.char_roles = {}
+        self.char_roles = {
+            role.name: role
+            for role in guild.roles
+            if role.name.lower() in CHARACTERS and role.members
+        }
