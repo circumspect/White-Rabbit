ecfa855c0dd0d8fe548f28752c7189d49d0df9ba
Chenkai <40770208+Chenkail@users.noreply.github.com>
2020-09-19 23:48:42 -0700

    Added basic clue card shuffle functionality

--- gamedata.py
+++ gamedata.py
@@ -14,10 +14,14 @@ CHARACTER_IMAGE_DIR = "Images/Cards/Characters"
 SUSPECT_IMAGE_DIR = "Images/Cards/Suspects"
 LOCATION_IMAGE_DIR = "Images/Cards/Locations"
 
+# Game length in seconds (90 minutes)
 GAME_LENGTH = 90 * 60
 # How often the bot should check the timer
 TIMER_GAP = 10
 
+# Times to send clue cards
+CLUE_TIMES = (90, 80, 70, 60, 50, 45, 40, 35, 30, 20)
+BUCKET_SIZES = {3: (3, 3, 4), 4: (2, 2, 3, 3), 5: (2, 2, 2, 2, 2)}
 
 class Data:
     def __init__(self, guild):
