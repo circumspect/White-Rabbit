9c7784e0f94a6f294578d345f6cac34e3a020570
circumspect <40770208+circumspect@users.noreply.github.com>
2020-09-21 19:43:01 -0700

    Add variables for suspects and locations

--- src/gamedata.py
+++ src/gamedata.py
@@ -9,14 +9,32 @@ CHARACTERS = {
     "julia": "Julia North",
 }
 
+SUSPECTS = {
+    "bria": "Bria Brown",
+    "cj": "CJ Wallace",
+    "david": "David Nelson",
+    "halvert": "Mr. Halvert",
+    "ryan": "Ryan Groggins",
+}
+
+LOCATIONS = {
+    "barn": "Barn",
+    "lighthouse": "Lighthouse",
+    "nightclub": "Nightclub",
+    "park": "State Park",
+    "station": "Train Station",
+}
+
 GAME_LENGTH = 90 * 60
 # How often the bot should check the timer
 TIMER_GAP = 10
 
-# Times to send clue cards
+# Clue card info
 CLUE_TIMES = (90, 80, 70, 60, 50, 45, 40, 35, 30, 20)
 BUCKET_SIZES = {3: (3, 3, 4), 4: (2, 2, 3, 3), 5: (2, 2, 2, 2, 2)}
-
+CLUE_TYPES = {80: "suspect", 70: "location", 60: "suspect", 50: "location",
+                45: ("suspect", "location", "location"), 40: "suspect", 
+                35: "location", 30: "suspect-drawn", 20: "location-drawn"}
 
 class Data:
     def __init__(self, guild):
@@ -38,6 +56,20 @@ class Data:
         # Create empty clue dicts
         self.clue_assignments = {}
         self.picked_clues = {}
+
+        # Suspects and locations
+        suspect_pile = []
+        for suspect in SUSPECTS.keys():
+            suspect_pile.append(suspect)
+            suspect_pile.append(suspect)
+
+        location_pile = []
+        for location in LOCATIONS.keys():
+            location_pile.append(location)
+            location_pile.append(location)
+
+        drawn_suspects = []
+        drawn_locations = []
 
     def char_roles(self):
         unsorted = {
