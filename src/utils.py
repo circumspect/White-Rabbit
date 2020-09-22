d153c209c3ef35888d88047d38d6ca92d1264a21
Creonalia <hyper.pyromaniac@simplelogin.co>
2020-09-21 22:47:14 -0700

    small refactorings

--- src/utils.py
+++ src/utils.py
@@ -49,7 +49,7 @@ def send_image(channel, filepath, ctx=None):
 
     if isinstance(channel, str):
         if not ctx:
-            raise ValueError
+            raise ValueError("Cannot send to channel without ctx.text_channels")
         channel = ctx.text_channels[channel]
     asyncio.create_task(channel.send(
         file=discord.File(filepath)
