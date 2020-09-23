47721d64da4a3f1f2d1c9ed2d4cc0b3788efd821
circumspect <40770208+circumspect@users.noreply.github.com>
2020-09-23 03:43:34 -0700

    Add settings.py

---
+++ src/settings.py
@@ -0,0 +1,62 @@
+# Built-in
+import asyncio
+# 3rd-party
+import discord
+from discord.ext import commands, tasks
+
+class Settings(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def auto(self, ctx, mode: str = ""):
+        """
+        Prints current mode or turn automatic on/off
+
+        Syntax:
+        !auto will display the current mode
+        !auto on will switch to automatic mode
+        !auto off will switch to manual mode
+        Automatic mode will disable manual card draw commands
+        """
+
+        if not mode:
+            # Print current mode
+            message = "```\nCurrent mode: "
+            if ctx.game.automatic:
+                message += "automatic"
+            else:
+                message += "manual"
+            message += "\n```"
+            await ctx.send(message)
+        elif mode == "on":
+            ctx.game.automatic = True
+            await ctx.send("Automatic card draw enabled!")
+        elif mode == "off":
+            ctx.game.automatic = False
+            await ctx.send("Automatic card draw disabled!")
+        else:
+            await ctx.send("Input error, try !auto on or !auto off")
+
+    @commands.command()
+    async def music(self, ctx):
+        """Enable/disable music stream when game starts"""
+
+        ctx.game.stream_music = not ctx.game.stream_music
+        if ctx.game.stream_music:
+            await ctx.send("Music stream enabled!")
+        else:
+            await ctx.send("Music stream disabled!")
+
+    @commands.command(name="timer")
+    async def show_timer(self, ctx):
+        """Show/hide bot timer"""
+
+        ctx.game.show_timer = not ctx.game.show_timer
+        if ctx.game.show_timer:
+            await ctx.send("Showing bot timer!")
+        else:
+            await ctx.send("Hiding bot timer!")
+
+def setup(bot):
+    bot.add_cog(Settings(bot))
