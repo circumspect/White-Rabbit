e9e25a88c268629041f4708a03e57abae23bd3c4
Chenkai <40770208+Chenkail@users.noreply.github.com>
2020-09-24 17:00:43 -0700

    Move !txt to export.py

--- src/admin.py
+++ src/admin.py
@@ -1,6 +1,5 @@
 # Built-in
 import asyncio
-import shutil
 from PIL import Image
 # 3rd-party
 import discord
@@ -36,41 +35,6 @@ class Admin(commands.Cog):
 
         # Reset game data
         ctx.game.__init__(ctx.game.guild)
-
-    @commands.command()
-    async def download(self, ctx):
-        """Gets all messages from a guild and writes to a .txt file"""
-
-        await ctx.send("Downloading...")
-        # make folder for messages
-        message_dir = utils.WHITE_RABBIT_DIR / ctx.guild.name
-        message_dir.mkdir(parents=True, exist_ok=True)
-
-        # Download messages
-        for channel in ctx.guild.text_channels:
-            messages = [
-                " ".join((
-                    message.created_at.strftime('%Y-%m-%d %H:%M'),
-                    message.author.display_name + ":",
-                    message.clean_content,
-                    ", ".join(attachment.url for attachment in message.attachments)
-                ))
-                async for message in channel.history(limit=None, oldest_first=True)
-            ]
-            with open(message_dir / f"{channel.name}.txt", mode="w") as message_file:
-                message_file.write("\n".join(messages))
-
-        # Send zip
-        zip_file = utils.WHITE_RABBIT_DIR / f"{ctx.guild.name} Messages.zip"
-        shutil.make_archive(
-            zip_file.with_suffix(""),
-            "zip", message_dir,
-        )
-        await ctx.send(file=discord.File(zip_file))
-
-        # Delete files
-        shutil.rmtree(message_dir)
-        zip_file.unlink()
 
 def setup(bot):
     bot.add_cog(Admin(bot))
