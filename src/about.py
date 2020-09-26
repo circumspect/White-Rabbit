e588ff0bb35cf4c758d7f1b859426dc12055256b
Chenkai <40770208+Chenkail@users.noreply.github.com>
2020-09-26 15:35:43 -0700

    Create !about and !docs

---
+++ src/about.py
@@ -0,0 +1,36 @@
+# Built-in
+import asyncio
+# 3rd-party
+from discord.ext import commands
+# Local
+import utils
+
+class About(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def about(self, ctx):
+        """Prints information about the bot"""
+
+        message = "\n".join([
+            "The White Rabbit was created by Chenkai and Chendi Luo.",
+            "Source code available at https://github.com/Chenkail/White-Rabbit"
+        ])
+        message = utils.codeblock(message)
+        asyncio.create_task(ctx.send(message))
+    
+    @commands.command(aliases=["guide"])
+    async def docs(self, ctx):
+        """Link to the documentation"""
+
+        message = "\n".join([
+            "Documentation for The White Rabbit can be found here:",
+            "https://white-rabbit.rtfd.io/"
+        ])
+        message = utils.codeblock(message)
+        asyncio.create_task(ctx.send(message))
+
+
+def setup(bot):
+    bot.add_cog(About(bot))
