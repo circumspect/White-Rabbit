c03cfe54961e15e67f133d9ba163c0f4b5c98bfb
circumspect <40770208+circumspect@users.noreply.github.com>
2020-09-26 19:52:01 -0700

    Move master file to index

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,3 +54,7 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+# Set master doc so readthedocs build doesn't fail
+master_doc = 'index'
