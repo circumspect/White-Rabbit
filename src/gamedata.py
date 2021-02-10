b884fc64b10aa6f533dfb847860dafb0dd5bd407
Chenkail <40770208+Chenkail@users.noreply.github.com>
2021-02-09 18:58:02 -0800

    Fix docstring

--- src/gamedata.py
+++ src/gamedata.py
@@ -142,7 +142,7 @@ class Data:
 
     def char_roles(self):
         """
-        Returns a dictionary mapping lowercase character names to their corresponding
+        Returns a dictionary mapping titlecase character names to their corresponding
         roles for all characters currently being played
         """
 
