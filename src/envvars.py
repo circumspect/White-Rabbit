4cdbbf1a48d99dd8dc952db37d8717a545d0e9d3
Chenkail <40770208+circumspect@users.noreply.github.com>
2021-03-27 08:02:17 -0700

    Fix character introductions directory in code

--- src/envvars.py
+++ src/envvars.py
@@ -6,6 +6,7 @@ from dotenv import dotenv_values
 
 def get_env_var(key: str):
     var = environ.get(key) or dotenv_values(".env")[key]
+    var = var.strip()
     if var.lower() == "false":
         return False
 
