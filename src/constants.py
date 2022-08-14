d9e7136c7f830c5b59a5a8b65e9d0326143b38dc
circumspect <40770208+circumspect@users.noreply.github.com>
2022-08-14 12:53:49 -0700

    chore: cleanup

--- src/constants.py
+++ src/constants.py
@@ -7,8 +7,8 @@ COMMAND_PREFIX = "!"
 VERSION = "0.11.0"
 DOCS_URL = "https://white-rabbit.readthedocs.io/"
 DOCS_SHORT_URL = "https://white-rabbit.rtfd.io/"
-SOURCE_URL = "https://github.com/d-beezee/White-Rabbit"
-RAW_FILES_URL = f"https://raw.githubusercontent.com/d-beezee/White-Rabbit/{VERSION}/"
+SOURCE_URL = "https://github.com/circumspect/White-Rabbit"
+RAW_FILES_URL = f"https://raw.githubusercontent.com/circumspect/White-Rabbit/{VERSION}/"
 BLANK_DOTENV_URL = f"{RAW_FILES_URL}/example.env"
 
 # Console logging message stuff
