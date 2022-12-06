0922133fa1e90cbd5e3086e9ed2eda1247306b99
circumspect <40770208+circumspect@users.noreply.github.com>
2022-12-06 23:55:50 +0100

    chore: bump version to 0.12.5

--- src/constants.py
+++ src/constants.py
@@ -4,7 +4,7 @@
 COMMAND_PREFIX = "!"
 
 # Links and bot info
-VERSION = "0.12.4"
+VERSION = "0.12.5"
 DOCS_URL = "https://white-rabbit.readthedocs.io/"
 DOCS_SHORT_URL = "https://white-rabbit.rtfd.io/"
 SOURCE_URL = "https://github.com/circumspect/White-Rabbit"
