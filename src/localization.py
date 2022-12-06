e3665a4ca2b5da148b58d313e44a72d2028c210e
circumspect <40770208+circumspect@users.noreply.github.com>
2022-12-06 23:38:59 +0100

    feat: use fstrings in localization (#120)

--- src/localization.py
+++ src/localization.py
@@ -1,6 +1,7 @@
 # Built-in
 import json
 # Local
+import cards
 import envvars
 
 # White-Rabbit/src/localization.py
@@ -13,7 +14,6 @@ LOCALIZATION_DIR = WHITE_RABBIT_DIR / "localization"
 LOCALIZATIONS = {
     "en": "english.json",
     "fr": "french.json",
-    "fr-2": "french-2.json",
     "de": "german.json",
     "it": "italian.json",
 }
@@ -27,5 +27,19 @@ print(f"Loading localization data ({LANGUAGE_KEY})... ", end="")
 LOCALIZATION_DATA = None
 with open(localization_file, encoding='utf-8') as f:
     LOCALIZATION_DATA = json.load(f)
+
+LOCALIZATION_DATA["channels"]["clues"] = {}
+characters = [name for name in cards.CHARACTERS]
+for character in characters:
+    LOCALIZATION_DATA["channels"]["clues"][character] = LOCALIZATION_DATA["channels"]["clue-channel-format"].format(character)
+
+LOCALIZATION_DATA["channels"]["texts"] = {}
+LOCALIZATION_DATA["channels"]["texts"]["group-chat"] = LOCALIZATION_DATA["channels"]["group-chat"]
+
+for i, char1 in enumerate(characters):
+    # Create list of character pairs
+    for j in range(i+1, len(characters)):
+        char2 = characters[j]
+        LOCALIZATION_DATA["channels"]["texts"][f"{char1}-{char2}"] = LOCALIZATION_DATA["channels"]["pm-channel-format"].format(char1, char2)
 
 print("Done!")
