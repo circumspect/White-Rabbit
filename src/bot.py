57167310a2f74df084151eb7277d259d9f0cb63a
circumspect <40770208+circumspect@users.noreply.github.com>
2023-02-25 12:52:58 +0100

    style: cleanup

--- src/bot.py
+++ src/bot.py
@@ -2,9 +2,6 @@ from discord.ext import commands
 
 class WhiteRabbit(commands.Bot):
     async def setup_hook(self) -> None:
-        # here, we are loading extensions prior to sync to ensure we are syncing interactions defined in those extensions.
-
-        # Load all extensions
         PLUGINS = ["about", "admin", "debug", "export", "game", "manual", "players", "settings"]
         for plugin in PLUGINS:
             await self.load_extension(f"cogs.{plugin}")
