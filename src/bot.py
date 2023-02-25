52b91956f9f88ae767f691f28749335d7ed11297
circumspect <40770208+circumspect@users.noreply.github.com>
2023-02-25 12:34:05 +0100

    chore: clean up bot.py

--- src/bot.py
+++ src/bot.py
@@ -1,14 +1,4 @@
-# Built-in
-
-# 3rd-party
 from discord.ext import commands
-
-# Local
-from localization import LOCALIZATION_DATA
-
-# Localization
-BOT_CHANNEL = LOCALIZATION_DATA["channels"]["bot-channel"]
-SPECTATOR_ROLE = LOCALIZATION_DATA["spectator-role"]
 
 class WhiteRabbit(commands.Bot):
     async def setup_hook(self) -> None:
@@ -18,4 +8,3 @@ class WhiteRabbit(commands.Bot):
         PLUGINS = ["about", "admin", "debug", "export", "game", "manual", "players", "settings"]
         for plugin in PLUGINS:
             await self.load_extension(f"cogs.{plugin}")
-
