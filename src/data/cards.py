b0adedbef10a623c3f13ff19710e3e9b90e25524
circumspect <40770208+circumspect@users.noreply.github.com>
2023-02-26 12:11:54 +0100

    feat: randomly choose five locations/suspects from available pool

--- src/data/cards.py
+++ src/data/cards.py
@@ -1,16 +1,24 @@
-from rabbit import WHITE_RABBIT_DIR
+# Built-in
 import envvars
+import random
 import yaml
+# Local
+from rabbit import WHITE_RABBIT_DIR
+
 
 CARD_LIST_DIR = WHITE_RABBIT_DIR / "card_lists"
 
 with open(CARD_LIST_DIR / f"{envvars.get_env_var('WHITE_RABBIT_CARD_LIST')}.yaml", "r") as f:
-    data = yaml.safe_load(f)
+    CARD_LIST = yaml.safe_load(f)
 
-CHARACTERS = data["characters"]
+CHARACTERS = CARD_LIST["characters"]
 ROLES_TO_CHARACTERS = {
     details["role"]: character for character, details in CHARACTERS.items()
 }
-SUSPECTS = data["suspects"]
-LOCATIONS = data["locations"]
-SEARCHING = data["searching"]
+ALL_SUSPECTS = CARD_LIST["suspects"]
+suspect_keys = random.sample(ALL_SUSPECTS.keys(), 5)
+SUSPECTS = { k: ALL_SUSPECTS[k] for k in suspect_keys }
+ALL_LOCATIONS = CARD_LIST["locations"]
+location_keys = random.sample(ALL_LOCATIONS.keys(), 5)
+LOCATIONS = { k: ALL_LOCATIONS[k] for k in location_keys }
+SEARCHING = CARD_LIST["searching"]
