38a13f76aea5d758d98a782fc1d3c686d248ea1a
circumspect <40770208+circumspect@users.noreply.github.com>
2023-02-27 14:12:42 +0100

    feat(playsets): read each expansion

--- src/data/cards.py
+++ src/data/cards.py
@@ -8,9 +8,19 @@ from rabbit import WHITE_RABBIT_DIR
 
 
 CARD_LIST_DIR = WHITE_RABBIT_DIR / "card_lists"
+EXPANSION_DIR = CARD_LIST_DIR / "expansions"
+PLAYSET_DIR = CARD_LIST_DIR / "playsets"
 
-with open(CARD_LIST_DIR / f"{envvars.get_env_var('WHITE_RABBIT_CARD_LIST')}.yaml", "r") as f:
-    CARD_LIST = yaml.safe_load(f)
+expansions: List[str] = []
+with open(PLAYSET_DIR / f"{envvars.get_env_var('WHITE_RABBIT_PLAYSET')}.yaml", "r") as f:
+    for line in f:
+        line = line.strip()
+        if line:
+            expansions.append(line)
+
+for expansion in expansions:
+    with open(CARD_LIST_DIR / f"{expansion}.yaml", "r") as f:
+        CARD_LIST = yaml.safe_load(f)
 
 CHARACTERS: Dict[str, Character] = { k: Character(k, v) for k, v in CARD_LIST["characters"].items() }
 ROLES_TO_NAMES = { v.role: v.name for _, v in CHARACTERS.items() }
