f6617f4ea6526d76b0bdbccdbf51f0adbf60250e
circumspect <40770208+circumspect@users.noreply.github.com>
2023-04-05 11:52:51 +0200

    feat(logging): use log level inheritance

--- src/logger.py
+++ src/logger.py
@@ -5,16 +5,15 @@ import discord
 
 LOGGING_FORMATTER = discord.utils._ColourFormatter()
 LOGGING_HANDLER = logging.StreamHandler()
-LOGGING_HANDLER.setLevel(logging.INFO)
 LOGGING_HANDLER.setFormatter(LOGGING_FORMATTER)
 
 
 def get_logger(name: str):
     if name == "__main__":
         logger = logging.getLogger(f"white-rabbit")
+        logger.setLevel(logging.INFO)
     else:
         logger = logging.getLogger(f"white-rabbit.{name}")
-    logger.setLevel(logging.INFO)
     logger.addHandler(LOGGING_HANDLER)
 
     return logger
