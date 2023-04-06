f9aadfb4cbc2f4caf607dd21bd18c1accff530b4
circumspect <40770208+circumspect@users.noreply.github.com>
2023-04-06 23:48:40 +0200

    fix(stats): fix broken counters

--- src/stats.py
+++ src/stats.py
@@ -1,20 +1,22 @@
 import requests
 
 API = "https://api.countapi.xyz/hit/white-rabbit-bot/"
-GAMES_PLAYED_COUNTER = API + "games-played"
-PDF_EXPORT_COUNTER = API + "pdfs-created"
-COMMANDS_RUN_COUNTER = API + "commands-run"
+GAMES_PLAYED = "games-played"
+PDF_EXPORT = "pdfs-created"
+COMMANDS_RUN = "commands-run"
+PLAYER_MESSAGES = "player-messages"
 
 def increment(counter: str):
     requests.get(API + counter)
 
 def increment_games_started(lang: str):
-    increment(GAMES_PLAYED_COUNTER)
+    increment(GAMES_PLAYED)
     if lang:
-        increment(f"{GAMES_PLAYED_COUNTER}-{lang}")
+        increment(f"{GAMES_PLAYED}-{lang}")
 
 def increment_pdfs():
-    increment(PDF_EXPORT_COUNTER)
+    increment(PDF_EXPORT)
 
 def increment_commands_run():
-    increment(COMMANDS_RUN_COUNTER)
+    increment(COMMANDS_RUN)
+
